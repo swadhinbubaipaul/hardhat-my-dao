372d5c7ae5bbf916d4589a6d9f8f5fed5eb15dd6
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-03-07 21:26:27 +0530

    changes in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,8 +10,8 @@ require("hardhat-deploy");
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const POLYGON_RPC_URL =
-	process.env.POLYGON_RPC_URL ||
+const MUMBAI_RPC_URL =
+	process.env.MUMBAI_RPC_URL ||
 	"https://eth-goerli.alchemyapi.io/v2/your-api-key";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
@@ -29,11 +29,11 @@ module.exports = {
 			blockConfirmations: 1,
 			allowUnlimitedContractSize: true,
 		},
-		polygon: {
-			url: POLYGON_RPC_URL,
+		mumbai: {
+			url: MUMBAI_RPC_URL,
 			accounts: [PRIVATE_KEY],
 			saveDeployments: true,
-			chainId: 5,
+			chainId: 80001,
 			blockConfirmations: 6,
 		},
 	},
@@ -41,6 +41,12 @@ module.exports = {
 		compilers: [
 			{
 				version: "0.8.9",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 100,
+					},
+				},
 			},
 		],
 	},
